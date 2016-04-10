6c0411c57bf1bfd06fedfd29770000d6a0944c0d
zaki-yama <shingoyamazaki00@gmail.com>
2016-04-11 07:46:32 +0900

    use webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
+const webpack = require('webpack');
+
 module.exports = {
   entry: __dirname + "/index.js",
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js'
   },
+
+  // Configuration for dev server
+  devServer: {
+    contentBase: 'dist',
+    devtool: 'eval',
+    hot: true,
+    inline: true,
+    port: 3000,
+  },
+
+  plugins: [
+    // Allows for sync with browser while developing (like BorwserSync)
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devtool: 'source-map',
   module: {
     loaders: [
       {
