68131804ca29196e570644d300e8d67743f3c35a
zaki-yama <shingoyamazaki00@gmail.com>
2016-04-10 15:13:49 +0900

    App.jsをclassに

--- components/App.js
+++ components/App.js
@@ -3,12 +3,14 @@ import Footer from './Footer';
 import AddTodo from '../containers/AddTodo';
 import VisibleTodoList from '../containers/VisibleTodoList';
 
-const App = () => (
-  <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
-  </div>
-);
-
-export default App;
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <AddTodo />
+        <VisibleTodoList />
+        <Footer />
+      </div>
+    );
+  }
+}
